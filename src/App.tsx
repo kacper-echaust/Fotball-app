2d57debd85fa6be9beec971741c2e3d053a478bd
kacper-echaust <k9echaust@gmail.com>
2025-03-25 18:35:45 +0100

    add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyles } from './Components/ui/GlobalStyles/GlobalStyles'
+
+const queryClient = new QueryClient()
+
 const App = () => {
-  return (<></>)
+	const theme = {
+		colors: {
+			primary: '#FF6B00',
+			secondary: '#0077FF',
+		},
+	}
+	return (
+		<QueryClientProvider client={queryClient}>
+			<ThemeProvider theme={theme}>
+				<GlobalStyles />
+			</ThemeProvider>
+		</QueryClientProvider>
+	)
 }
 
-export {App}
+export { App }
